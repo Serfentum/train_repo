64bbb45bbf2d4486caf93f053084e473743e9ca8
arleg <tetpapx@bk.ru>
2018-04-07 16:51:36 +0300

    Remastered class of trimmomatic

--- trimmomaticX/trimmomatic.py
+++ trimmomaticX/trimmomatic.py
@@ -6,54 +6,70 @@ from Bio.Seq import Seq
 
 class Trimmomatic:
 
-    def __init__(self, path, out):
-        self.path = ''
-        self.out = ''
-        self.sequences = []
+    def __init__(self, path, out='~/train_repo/trimmomaticX/trimmed.fastq'):
+        self.sequences = list(SeqIO.parse(path, 'fastq'))
+        self.out = out
         self.seqs = []
 
-    def process_input(self):
-        self.sequences = SeqIO.parse(self.path, 'fastq')
-
     def dump(self):
-        SeqIO.write(self.seqs, self.out, 'fastq')
+        SeqIO.write(self.sequences, self.out, 'fastq')
 
     def headcrop(self, sequence, size):
         # Drop size nucls from start
         # Create SeqRecord
-
-        self.seqs.append(SeqRecord(Seq(str(sequence.seq)[size:]),
-                                   id=sequence.id,
-                                   name=sequence.name,
-                                   description=sequence.description,
-                                   letter_annotations={spec: vals[size:] for spec, vals in
-                                                       sequence.letter_annotations.items()}))
+        self.seqs.append(sequence[size:])
+        # self.seqs.append(SeqRecord(Seq(str(sequence.seq)[size:]),
+        #                            id=sequence.id,
+        #                            name=sequence.name,
+        #                            description=sequence.description,
+        #                            letter_annotations={spec: vals[size:] for spec, vals in
+        #                                                sequence.letter_annotations.items()}))
 
         # sequence.letter_annotations['phred_quality'] = sequence.letter_annotations['phred_quality'][size:]
         # sequence.seq = str(sequence.seq)[size:]
 
-
-
     def tailcrop(self, sequence, size):
         # Drop size nucls from end
+        self.seqs.append(sequence[:-size])
+        # self.seqs.append(SeqRecord(Seq(str(sequence.seq)[:-size]),
+        #                            id=sequence.id,
+        #                            name=sequence.name,
+        #                            description=sequence.description,
+        #                            letter_annotations={spec: vals[:-size] for spec, vals in
+        #                                                sequence.letter_annotations.items()}))
 
-        self.seqs.append(SeqRecord(Seq(str(sequence.seq)[:-size]),
-                                   id=sequence.id,
-                                   name=sequence.name,
-                                   description=sequence.description,
-                                   letter_annotations={spec: vals[:-size] for spec, vals in
-                                                       sequence.letter_annotations.items()}))
-
-    def quality_crop(sequence, sliding_window, quality):
-        #
+    def quality_crop(self, sequence, sliding_window, quality):
         for i in range(len(sequence) - sliding_window + 1):
+            print(sequence.letter_annotations['phred_quality'][i:i + sliding_window])
             if mean(sequence.letter_annotations['phred_quality'][i:i + sliding_window]) < quality:
-                sequence.seq = str(sequence.seq)[:i]
+                j = i
+                break
+        self.seqs.append(sequence[0:i + sliding_window]
+                         if mean(sequence.letter_annotations['phred_quality'][i:i + sliding_window]) >= quality
+                         else sequence[0:i])
 
-    def pack_task(task, *args):
-        return lambda x: task(x, *args)
+    def pack_task(self, task, *args):
+        if [i for i in args if i]:
+            return lambda x: task(x, *args)
 
-    def do(sequences, tasks):
+    def do(self, tasks):
         for task in tasks:
-            for sequence in sequences:
-                task(sequence)
+            if task:
+                for sequence in self.sequences:
+                    task(sequence)
+
+                self.sequences = self.seqs
+                self.seqs = []
+
+
+a = Trimmomatic('/home/arleg/train_repo/trimmomaticX/test', 'out')
+g = a.sequences # seq
+
+for i in a.sequences:
+    print(i.letter_annotations['phred_quality'])
+
+print(list(g))
+
+a.do([a.pack_task(a.tailcrop, 5), a.pack_task(a.headcrop, 5), a.pack_task(a.quality_crop, 3, 38)])
+print(a.sequences)
+a.dump()
